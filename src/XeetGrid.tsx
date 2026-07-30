491802040525e5c9629c5d7e057f69db419d535d
Ray Legion <ray.chen@virovek.com>
2026-07-30 17:12:56 -0500

    commit for now cuz the transform stuff is kinda cool but nah i want to go with something else. also xeet grid. i probably will just embed tweets like normal

--- src/XeetGrid.tsx
+++ src/XeetGrid.tsx
@@ -1,92 +1,32 @@
 import { useState } from "react"
-import ray from "./assets/ray.png"
-import zal from "./assets/zal.png"
-import soneji from "./assets/soneji.png"
-// import obamium from "./assets/obamium.jpeg"
-// import yiv from "./assets/yiv.png"
-import yang from "./assets/yang.png"
-import lightfiend from "./assets/lightfiend.png"
 
-function BandGrid() {
-  const members = [
+
+function XeetGrid() {
+  const xeets = [
     {
-      name: "Ray", instrument: "Drums", img: ray, socials: {
-        yt: 'https://youtube.com/@raidsrc',
-        ig: 'https://instagram.com/raidsrc',
-        x: 'https://x.com/raidsrc',
-      },
-    },
-    {
-      name: "Yang", instrument: "Keyboard", img: yang, socials: {
-        ig: 'https://example.com',
-      },
-    },
-    {
-      name: "Lightfiend", instrument: "Bass", img: lightfiend, socials: {
-        x: 'https://x.com/lxtfnd',
-      },
-    },
-    {
-      name: "Ninomae Nimo", instrument: "Guitar", img: null, socials: {
-        x: 'https://x.com/ninomaenimo',
-      },
-    },
-    {
-      name: "Yiv", instrument: "Guitar", img: null, socials: {
-        x: 'https://x.com/yiversonh',
-      },
-    },
-    {
-      name: "Ang", instrument: "Trombone", img: null, socials: {
-        x: 'https://x.com/absoluteangles',
-      },
-    },
-    {
-      name: "Soneji", instrument: "Tenor saxophone", img: soneji, socials: {
-        web: 'https://soneji.moe',
-        x: 'https://x.com/justsoneji',
-      },
-    },
-    {
-      name: "Zal", instrument: "Trumpet", img: zal, socials: {
-        x: 'https://x.com/zalmusicyt',
-      },
-    },
-    {
-      name: "Harrison Fjord", instrument: "Keyboard", img: null, socials: {
-        x: 'https://x.com/harrison_fjord',
-      },
+      user:"",
+      text: ""
     },
   ]
   return (
     <>
       {
-        members.map((member) => {
-          return <BandMemberComponent key={member.name} member={member} />
+        xeets.map((xeet) => {
+          return <XeetComponent key={xeet.user} xeet={xeet} />
         })
       }
     </>
   )
 }
 
-function BandMemberComponent({ member }: {
-  member: Record<string, string | unknown>
+function XeetComponent({ xeet }: {
+  xeet: Record<string, string>
 }) {
-  const [hoverStyleClass, setHoverStyleClass] = useState("hidden")
-  const name = member.name as string
-  const instrument = member.instrument as string
-  const img = member.img as string
-  const socials = member.socials as Record<string, string>
-  const handleMouseover = () => {
-    setHoverStyleClass("visible")
-  }
-  const handleMouseout = () => {
-    setHoverStyleClass("hidden")
-  }
+  // probably just do a tweet embed like from x.com 
   return (
-    <div className="member-component" id={`${name.toLocaleLowerCase()}-member-component`} >
-      <div className="image-and-overlaid-text" onMouseOver={handleMouseover} onMouseOut={handleMouseout} >
-        <img src={img} id={`${name.toLocaleLowerCase()}-img`}/>
+    <div className="xeet-component" >
+      <div>
+        <img src={img} />
         <div className={`text ${hoverStyleClass}`}>
           <div>{name}</div>
           <div>{instrument}</div>
