d18ff07861bfbc2076bd4796cb6c80fdb52fe887
Ray Legion <ray.chen@virovek.com>
2026-07-29 23:46:13 -0500

    getting some band photos

--- src/BandGrid.tsx
+++ src/BandGrid.tsx
@@ -1,27 +1,59 @@
 import { useState } from "react"
+import ray from "./assets/ray.png"
+import zal from "./assets/zal.png"
+import soneji from "./assets/soneji.png"
+import obamium from "./assets/obamium.jpeg"
+import yiv from "./assets/yiv.png"
+import yang from "./assets/yang.png"
+import lightfiend from "./assets/lightfiend.png"
 
 function BandGrid() {
   const members = [
     {
-      name: "Ray", instrument: "Drums", socials: {
+      name: "Ray", instrument: "Drums", img: ray, socials: {
         yt: 'https://youtube.com/@raidsrc',
-        x: 'https://x.com/raidsrc',
         ig: 'https://instagram.com/raidsrc',
+        x: 'https://x.com/raidsrc',
       },
     },
     {
-      name: "Yang", instrument: "Keyboard", socials: {
+      name: "Yang", instrument: "Keyboard", img: yang, socials: {
         ig: 'https://example.com',
       },
     },
     {
-      name: "Lightfiend", instrument: "Bass", socials: {
+      name: "Lightfiend", instrument: "Bass", img: lightfiend, socials: {
         x: 'https://x.com/lxtfnd',
       },
     },
     {
-      name: "Lightfiend", instrument: "Bass", socials: {
-        x: 'https://x.com/lxtfnd',
+      name: "Ninomae Nimo", instrument: "Guitar", img: obamium, socials: {
+        x: 'https://x.com/ninomaenimo',
+      },
+    },
+    {
+      name: "Yiv", instrument: "Guitar", img: yiv, socials: {
+        x: 'https://x.com/yiversonh',
+      },
+    },
+    {
+      name: "Ang", instrument: "Trombone", img: obamium, socials: {
+        x: 'https://x.com/absoluteangles',
+      },
+    },
+    {
+      name: "Soneji", instrument: "Tenor saxophone", img: soneji, socials: {
+        x: 'https://x.com/justsoneji',
+      },
+    },
+    {
+      name: "Zal", instrument: "Trumpet", img: zal, socials: {
+        x: 'https://x.com/zalmusicyt',
+      },
+    },
+    {
+      name: "Harrison Fjord", instrument: "Keyboard", img: obamium, socials: {
+        x: 'https://x.com/harrison_fjord',
       },
     },
   ]
@@ -42,6 +74,7 @@ function BandMemberComponent({ member }: {
   const [hoverStyleClass, setHoverStyleClass] = useState("hidden")
   const name = member.name as string
   const instrument = member.instrument as string
+  const img = member.img as string
   const socials = member.socials as Record<string, string>
   const handleMouseover = () => {
     setHoverStyleClass("visible")
@@ -52,7 +85,7 @@ function BandMemberComponent({ member }: {
   return (
     <div className="member-component" >
       <div className="image-and-overlaid-text">
-        <img src="./assets/obamium.jpeg" onMouseOver={handleMouseover} onMouseOut={handleMouseout} />
+        <img src={img} onMouseOver={handleMouseover} onMouseOut={handleMouseout} />
         <div className={`text ${hoverStyleClass}`}>
           <div>{name}</div>
           <div>{instrument}</div>
